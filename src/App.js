06e6e8d84e8f508beb9b83b3990878675ba80930
Judith Maman <judith.maman@cybereason.com>
2022-02-16 01:18:00 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          More from Jud Maman Edit <code>src/App.js</code> and save to reload.
+          More and more from Jud Maman Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
